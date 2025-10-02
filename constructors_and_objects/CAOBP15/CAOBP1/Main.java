9583687f9b0bf97e15b762a29de4577d80a68666
Saint一切顺利 <188669077+Sanctana@users.noreply.github.com>
2025-10-02 16:04:08 +0800

    Update Main.java

--- constructors_and_objects/CAOBP15/CAOBP1/Main.java
+++ constructors_and_objects/CAOBP15/CAOBP1/Main.java
@@ -1,1 +1,9 @@
-hello
+public class Main{
+    public static void main(String[] args){
+        Player p1 = new Player("Saint", 5, 100);
+        Player p2 = new Player("Tria", 10, 50);
+        
+        p1.showStats();
+        p2.showStats();
+    }
+}

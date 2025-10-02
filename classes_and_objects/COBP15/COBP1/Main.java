4203bfe70fff95a07a73e125ce339cf0380ca2ec
Saint一切顺利 <188669077+Sanctana@users.noreply.github.com>
2025-10-02 08:43:34 +0800

    Create Main.java

---
+++ classes_and_objects/COBP15/COBP1/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        PlayerCharacter playerCharacter = new PlayerCharacter();
+        playerCharacter.name = "Alice";
+    
+        playerCharacter.attack();
+        playerCharacter.heal();
+    }
+}

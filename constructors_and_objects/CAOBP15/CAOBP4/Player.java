29722fa9b9b66d290b17547a9a15cddd9fabb1b6
Saint一切顺利 <188669077+Sanctana@users.noreply.github.com>
2025-10-03 11:51:08 +0800

    Create Player.java

---
+++ constructors_and_objects/CAOBP15/CAOBP4/Player.java
@@ -0,0 +1,11 @@
+class Player{
+    String name;
+    int attackPower;
+    int health;
+    
+    Player(String name, int attackPower, int health){
+        this.name = name;
+        this.attackPower = attackPower;
+        this.health = health;
+    }
+}
